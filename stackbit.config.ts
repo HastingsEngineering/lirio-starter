189f08e5a85fe7fd8cc47e9e2ef2f3480a12e080
nrage <nmthastings@protonmail.com>
2024-06-29 12:52:52 -0400

    Update stackbit config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,3 +1,19 @@
 import { lirioConfig } from "lirio/stackbit";
 
-export default { ...lirioConfig(__dirname), useESM: true};
+export default { 
+    stackbitVersion: '~0.6.0',
+    nodeVersion: '18',
+    ssgName: 'custom',
+    devCommand: './node_modules/.bin/vite --port {PORT}',
+    experimental: {
+        ssg: {
+            name: 'sveltekit',
+            logPatterns: {
+                up: [' ready in '],
+            },
+            passthrough: ['/vite-hmr/**'],
+        },
+    },
+    ...lirioConfig(__dirname), 
+    useESM: true
+};
